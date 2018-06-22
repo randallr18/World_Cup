e7762bb5cc30a77a7a5dbb055bc73e6126fbc11e
Risher Randall <randallr18@mail.wlu.edu>
2018-06-21 20:04:57 -0400

    FINIALLYYY

--- bin/run.rb
+++ bin/run.rb
@@ -7,4 +7,14 @@ require_relative '../app/models/team.rb'
 require_relative '../app/models/match.rb'
 require_relative '../app/models/user_methods.rb'
 
+
+Stadium.add_stadiums_from_API
+Team.add_teams_from_API
+Match.add_hometeam_matches_from_API
+Match.add_awayteam_matches_from_API
+
 runner
+
+Stadium.delete_all
+Team.delete_all
+Match.delete_all
